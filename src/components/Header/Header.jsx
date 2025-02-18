c10b7edf8cefde5585f0f203114e57c7cef42c85
Optimus Elikem <portiaadaletey@gmail.com>
2025-02-18 13:13:34 +0000

    added header and explore-menu components, added their various styles and responsiveness

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css"
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="header-contents">
+                <h2>Order Your Favourite Food Here</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, dolores alias sint consectetur optio vero dignissimos temporibus natus beatae dolorum!</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,15 @@
-import Navbar from "../../components/Navbar/Navbar"
+import Navbar from "../../components/Navbar/Navbar";
+import Header from "../../components/Header/Header";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 
 function Home() {
-    return(
-        <Navbar />
-    )
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <ExploreMenu />
+    </>
+  );
 }
 
 export default Home;
